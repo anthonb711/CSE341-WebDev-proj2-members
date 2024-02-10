69682fbc21a20bf78b8f187a59c0f1ca4b72e226
anthonb711 <anthonb711@gmail.com>
2024-02-10 16:14:47 -0500

    functioning getMemebrById route on local, and swagger update

--- controllers/membersController.js
+++ controllers/membersController.js
@@ -1,8 +1,9 @@
 
 const mongodb= require('../db/connect');
-const ObjectId = require('mongodb').ObjectId;
 const Member = require('../db/memberSchema');
 
+
+//GETTERS
 
 const getMembers = async (req, res) => {
     try {
@@ -10,10 +11,23 @@ const getMembers = async (req, res) => {
          res.status(200).json(allMembers);
     } catch (error) {
         console.error('Error fetching members:', error);
-        res.status(500),json({ error: 'Internal Server Error'});
+        res.status(500).json({ error: 'Internal Server Error'});
     }
 };
 
+ const getMemberById = async (req, res) => {
+    const memberId = req.params.id;
+    try {
+        const thisMember = await Member.findById(memberId);
+        res.status(200).json(thisMember);
+    } catch (error) {
+        console.error('Error fetching member by ID:');
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+ };
+
+
+ // POSTS
 const addMember = async (req, res) => {
     try {
         const { lname, fname, gender, age, lastPrayer, lastTalk } = req.body;
@@ -34,7 +48,18 @@ const addMember = async (req, res) => {
 };
 
 
+// PUTS
+
+/* routes.put('/:id', controller.updateMemberRecord);    */
+
+//DELETES
+
+/* routes.delete('/:id', controller.deleteMember); */
+
 module.exports = { 
     getMembers,
-    addMember
+    getMemberById,
+    addMember,
+    /* updateMemberRecord, */
+    /* deleteMember, */
  };
