56df44193949e5cb0b700b7102d048b1ffe6ad66
anthonb711 <anthonb711@gmail.com>
2024-02-24 21:22:23 -0500

    adding the 'active' field to the memeberSchema

--- db/memberSchema.js
+++ db/memberSchema.js
@@ -5,6 +5,7 @@ const memberSchema = new mongoose.Schema({
   fname: { type: String, required: true },
   gender: { type: String, enum: ['M', 'F'], required: true },
   age: { type: Number, required: true, min: 0, max: 120 },
+  active: {type: String, enum: ['Y', 'N'], required: true},
   lastPrayer: { type: Date, default: null },
   lastTalk: { type: Date, default: null }
 });
