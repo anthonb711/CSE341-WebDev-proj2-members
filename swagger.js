71f8bc87162579b69831c2fbd92e3fecbcb28950
anthonb711 <anthonb711@gmail.com>
2024-03-02 12:21:25 -0500

    seting swagger host to render

--- swagger.js
+++ swagger.js
@@ -6,15 +6,12 @@ const doc = {
     description: 'Description',
     version: '1.0.1'
   },
-  //host: 'agbwebdev-proj2.onrender.com',
-  host: 'localhost:8080',
+  host: 'agbwebdev-proj2.onrender.com',
+  //host: 'localhost:8080',
   schemes: ['https']
 };
 
 const outputFile = './swagger-output.json';
 const routes = ['./routes/index.js'];
 
-swaggerAutogen(outputFile, routes, doc).then(() => {
-  const swaggerSpec = require(outputFile);
-  swaggerSpec.paths['/'].get.produces = ['text/html']; // Add this line to specify the expected media type
-});
+swaggerAutogen(outputFile, routes, doc);
