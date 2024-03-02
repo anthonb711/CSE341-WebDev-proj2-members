d261de70da8b9012af0e48cb54000b44e8ab7d9a
anthonb711 <anthonb711@gmail.com>
2024-03-02 11:28:02 -0500

    comment out the set header in server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use((req, res, next) => {
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
   );
-  res.setHeader('Content-Type', 'application/json');
+//   res.setHeader('Content-Type', 'application/json');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, FETCH');
   next();
 });
